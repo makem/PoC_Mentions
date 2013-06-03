e5e51ba17eeea157095b8a4a435be46a79fd2ab9
Maxim Vygovskiy <makem@ukr.net>
2013-06-03 09:42:18 -0700

    First success try
    Have tried http://mmgp.ru/showthread.php?t=19304&page=268"

---
+++ WebScan.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebScan.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = new HtmlDocument();
+            System.Console.OutputEncoding = Encoding.Unicode;
+            using (var client = new WebClient())
+            {
+                using (var stream = client.OpenRead("http://mmgp.ru/showthread.php?t=19304&page=268"))
+                {
+                    var reader = new StreamReader(stream, Encoding.GetEncoding("Windows-1251"));
+                    var html = reader.ReadToEnd();
+                    document.LoadHtml(html);
+                }
+            }
+            var selection = document.DocumentNode.SelectNodes(@"//div[starts-with(@id,'post_message_')]");
+            foreach (var node in selection)
+            {
+                System.Console.WriteLine("{0}", node.InnerText);
+            }
+            System.Console.WriteLine("привет");
+        }
+    }
+}
